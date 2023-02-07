adab15973ab292ff6ba93fbe790a74928bf92249
Feo Lee <musitlee@gmail.com>
2023-02-07 15:52:47 +0800

    graceful shutdown && iperf for ops image

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/feo0o/kyanite/server"
 )
 
 func main() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		fmt.Println("caught SIGTERM, shutting down")
+		os.Exit(0)
+	}()
+
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
